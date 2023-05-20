fa9605adf9dac1cd7f6c7fc9eda323b168ace479
Leyan <416138794@qq.com>
2023-05-20 21:02:53 +0800

    [feature]添加了发送最短路数据的功能

--- src/test/java/com/example/neo4jd3/TCPTests.java
+++ src/test/java/com/example/neo4jd3/TCPTests.java
@@ -1,11 +1,14 @@
 package com.example.neo4jd3;
 
+import com.google.gson.Gson;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.List;
+import java.util.Map;
 
 public class TCPTests {
     private Socket client;
@@ -16,17 +19,37 @@ public class TCPTests {
             client = new Socket("localhost", 65432);
             System.out.println("Connected to server");
 
-            setClientMessage();
+            setClientMessage("Hello from client");
         } catch (IOException e) {
             System.out.println("Could not connect to server");
         }
     }
 
-    void setClientMessage() {
+    @Test
+    public void testJson() {
+        Map<String, Object> map = Map.of(
+                "name", "st5",
+                "jointAngle", List.of(0.0, 0.0, 0.0, 0.0, 0.0, 0.0),
+                "position", "0,0,0,0,0,0"
+        );
+
+        Gson gson = new Gson();
+        String json = gson.toJson(map);
+
+        try {
+            client = new Socket("localhost", 65432);
+            System.out.println("Connected to server");
+
+            setClientMessage(json);
+        } catch (IOException e) {
+            System.out.println("Could not connect to server");
+        }
+    }
+
+    void setClientMessage(String message) {
         try {
             OutputStream pt = client.getOutputStream();
-            String printText = "Hello from client";
-            pt.write(printText.getBytes());
+            pt.write(message.getBytes());
 
             InputStream in = client.getInputStream();
             byte[] buffer = new byte[1024];
